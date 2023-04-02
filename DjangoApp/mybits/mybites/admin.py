ef0f8214aa6b7765726651c94468846d3bf2f43b
adriansanzzzz <74414073+adriansanzzzz@users.noreply.github.com>
2023-04-03 00:10:41 +0200

    admin new

--- DjangoApp/mybits/mybites/admin.py
+++ DjangoApp/mybits/mybites/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import * 
+from mybites.models import MyBite
 
+# Register your models here.
 
+admin.site.site_header = 'MyBites Administration'
 admin.site.register(Restaurant)
 admin.site.register(Localization)
 admin.site.register(Meal)
@@ -9,3 +11,7 @@ admin.site.register(Client)
 admin.site.register(Reservation)
 admin.site.register(Order)
 admin.site.register(Menu)
+
+
+
+
--- DjangoApp/mybits/mybits/settings.py
+++ DjangoApp/mybits/mybits/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-k+ke82km8#!-izz+@_z$e(q#sp3l((r6q@3oqxva=1um^u$3b0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
