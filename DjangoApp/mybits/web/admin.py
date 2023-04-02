75136f6c44f9069c24f92a7d8d9f98f267fd2238
adriansanzzzz <74414073+adriansanzzzz@users.noreply.github.com>
2023-04-03 01:48:51 +0200

    general update

--- DjangoApp/mybits/web/admin.py
+++ DjangoApp/mybits/web/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Restaurant, Localization, Meal, Client, Reservation, Order, Menu
-
+from .models import *
 
 
 admin.site.register(Restaurant)
---
+++ DjangoApp/mybits/web/migrations/0001_initial.py
@@ -0,0 +1,10 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+class Migration(migrations.Migration):
+    initial = True
+    dependencies = [
+    ]
+    
+    operations = [
+    ]
