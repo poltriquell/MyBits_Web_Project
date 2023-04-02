1a3bdaf4553e0fc59192b33801ff77bb7bdefa91
adriansanzzzz <74414073+adriansanzzzz@users.noreply.github.com>
2023-04-02 22:54:13 +0200

    admin

--- DjangoApp/mybits/mybites/admin.py
+++ DjangoApp/mybits/mybites/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from .models import * 
 
 
-admin.register(Restaurant)
-admin.register(Localization)
-admin.register(Meal)
-admin.register(Client)
-admin.register(Reservation)
-admin.register(Order)
-admin.register(Menu)
+admin.site.register(Restaurant)
+admin.site.register(Localization)
+admin.site.register(Meal)
+admin.site.register(Client)
+admin.site.register(Reservation)
+admin.site.register(Order)
+admin.site.register(Menu)
