8a93d57c65a4c3dafc9a984b36d388db29f1b0a7
adriansanzzzz <74414073+adriansanzzzz@users.noreply.github.com>
2023-04-04 11:42:59 +0200

    database postgresql working

--- DjangoApp/mybits/mybits/settings.py
+++ DjangoApp/mybits/mybits/settings.py
@@ -75,6 +75,7 @@ WSGI_APPLICATION = "mybits.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
