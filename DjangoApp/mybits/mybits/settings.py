ca7337aaf88d763a5b4ffe37d2a17c01d099117a
Enric-Tobena <etc7@alumnes.udl.cat>
2023-04-06 16:14:13 +0200

    COMMIT RANDOM

--- DjangoApp/mybits/mybits/settings.py
+++ DjangoApp/mybits/mybits/settings.py
@@ -17,13 +17,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import os
-SECRET_KEY = os.environ['SECRET_KEY']
+from django.core.management.utils import get_random_secret_key
+SECRET_KEY = "pfrkmgvr[+++[]o··]"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost","postgres"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
