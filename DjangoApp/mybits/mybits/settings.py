c96359f31c0843599a0f4fc9e02ffddf518f57b3
alex <alex.codinab@gmail.com>
2023-04-06 10:45:03 +0000

    key implemented

--- DjangoApp/mybits/mybits/settings.py
+++ DjangoApp/mybits/mybits/settings.py
@@ -17,7 +17,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-k+ke82km8#!-izz+@_z$e(q#sp3l((r6q@3oqxva=1um^u$3b0"
+import os
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
