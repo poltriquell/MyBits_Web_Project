d6ee4ab685d3a99f9697d1ed7d70dac9d73a2846
Enric-Tobena <etc7@alumnes.udl.cat>
2023-04-24 10:39:52 +0200

    It doesnt work. Upload to Github

--- DjangoApp/mybits/mybits/settings2.py
+++ DjangoApp/mybits/mybits/settings2.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework',
     "web",
 ]
 
@@ -81,7 +82,13 @@ DATABASES = {
     'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
 }
 
-
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 
 
--- DjangoApp/mybits/mybits/urls.py
+++ DjangoApp/mybits/mybits/urls.py
@@ -20,6 +20,25 @@ from django.conf import settings
 
 import web.views as wb
 
+from django.urls import path, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
 urlpatterns = [
     path('', wb.home, name='Home'),
     
@@ -43,10 +62,13 @@ urlpatterns = [
     
     path('login/', wb.login, name='login-menu'), 
     path('login/register/', wb.register, name='register-menu'),  
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls'),
     
     path('admin/', admin.site.urls),
-    
+   
     path('about_us/', wb.about, name='about-us'),
+    
 ]
 
 # path('search/', search_bar, name='content_search'),
