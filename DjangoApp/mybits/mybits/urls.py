6efad9503dd8c0c6ceca6a30c58888b324c1a76c
triquell02 <poltri02@gmail.com>
2023-05-06 18:08:15 +0200

    Delete all Bootstrap local libraries

--- DjangoApp/mybits/mybits/urls.py
+++ DjangoApp/mybits/mybits/urls.py
@@ -20,39 +20,44 @@ from django.conf import settings
 from web.views import add_restaurant, restaurant_list
 import web.views as wb
 
+from django.contrib.auth import views
+from django.views.static import serve
+from django.conf import settings
+
 urlpatterns = [
     path('', wb.home, name='Home'),
-    
+
     path('restaurant/', wb.restaurant_list, name='restaurant-list'),
     path('restaurant/<int:id_rest>/', wb.restaurant_detail, name='restaurant-detail'),
-    
+
     path('localization/', wb.localization_list, name='localization-list'),
     path('localization/<int:id_loc>/', wb.localization_detail, name='localization-detail'),
-    
+
     path('client/', wb.client_list, name='client-list'),
     path('client/<int:id_client>/', wb.client_detail, name='client-detail'),
-    
+
     path('reservation/', wb.reservation_list, name='reservation-list'),
     path('reservation/<int:id_book>/', wb.reservation_detail, name='reservation-detail'),
-    
+
     path('order/', wb.order_list, name='order-list'),
     path('order/<int:id_order>/', wb.order_detail, name='order-detail'),
-    
+
     path('menu/', wb.menu_list, name='menu-list'),
     path('menu/<int:id_menu>/', wb.menu_detail, name='menu-detail'),
-    
+
     path('login/', wb.login, name='login-menu'),
     path('login/register/', wb.register, name='register-menu'),
-    
+
     path('admin/', admin.site.urls),
-    
+
     path('about_us/', wb.about, name='about-us'),
-    
+
     path('restaurant/create/', wb.add_restaurant, name='restaurant'),
 
     path('accounts/login', wb.LoginView.as_view(), name='login'),
+    path('accounts/register', wb.register, name='register'),
     path('accounts/login', wb.LogoutView.as_view(), name='logout'),
-
+    
 ]
 
 # path('search/', search_bar, name='content_search'),
