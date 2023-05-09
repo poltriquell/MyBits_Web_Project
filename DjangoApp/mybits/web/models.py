bad32e67b0b6818e70c36155258f25ccdb8d1ed4
adriansanzzzz <74414073+adriansanzzzz@users.noreply.github.com>
2023-05-09 13:23:08 +0200

    map updates

--- DjangoApp/mybits/web/models.py
+++ DjangoApp/mybits/web/models.py
@@ -14,8 +14,6 @@ class Restaurant(models.Model):
 
 class Localization(models.Model):
     id_localization = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
-    address = models.CharField(max_length=50)
 
 class Client(models.Model):
     id_client = models.AutoField(primary_key=True)
