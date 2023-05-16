cff87edca8f1f4b9d4135ecd9b9503fbcb322f26
adriansanzzzz <74414073+adriansanzzzz@users.noreply.github.com>
2023-05-16 13:36:24 +0200

    DATABASE SOLVED

---
+++ DjangoApp/mybits/fulldata.py
@@ -0,0 +1,58 @@
+import os
+import django
+
+# Configurar la configuración de Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mybits.settings')
+
+django.setup()
+from web.models import Restaurant
+
+def populate_restaurants():
+    restaurantes = [
+        {
+            'name': 'Restaurante 1',
+            'phone': '123456789',
+            'email': 'restaurante1@example.com',
+            'website': 'www.restaurante1.com',
+            'longitude': 41.6001,
+            'latitude': 0.6227
+        },
+        {
+            'name': 'Restaurante 2',
+            'phone': '987654321',
+            'email': 'restaurante2@example.com',
+            'website': 'www.restaurante2.com',
+            'longitude': 41.9002,
+            'latitude': 0.9228
+        },
+        {
+            'name': 'Restaurante 3',
+            'phone': '456123789',
+            'email': 'restaurante3@example.com',
+            'website': 'www.restaurante3.com',
+            'longitude': 42.6003,
+            'latitude': 0.8229
+        },
+        # Agrega aquí los demás restaurantes
+        {
+            'name': 'Restaurante 12',
+            'phone': '753951456',
+            'email': 'restaurante12@example.com',
+            'website': 'www.restaurante12.com',
+            'longitude': 42.6011,
+            'latitude': 0.8237
+        }
+    ]
+    for restaurante in restaurantes:
+        Restaurant.objects.create(
+            name=restaurante['name'],
+            phone=restaurante['phone'],
+            email=restaurante['email'],
+            website=restaurante['website'],
+            longitude=restaurante['longitude'],
+            latitude=restaurante['latitude']
+        )
+
+
+if __name__ == '__main__':
+    populate_restaurants()
--- DjangoApp/mybits/web/models.py
+++ DjangoApp/mybits/web/models.py
@@ -8,8 +8,8 @@ class Restaurant(models.Model):
     phone = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
     website = models.CharField(max_length=50)
-    longitude = models.FloatField(default=41.6)  # Define tu valor predeterminado aquí
-    latitude = models.FloatField(default=0.6226)  # Define tu valor predeterminado aquí
+    longitude = models.FloatField(max_length=50)  # Define tu valor predeterminado aquí
+    latitude = models.FloatField(max_length=50)  # Define tu valor predeterminado aquí
     
 class Menu(models.Model):
     id_menu = models.AutoField(primary_key=True, default=1)
@@ -38,7 +38,6 @@ class Reservation(models.Model):
     id_client = models.ForeignKey(Client, on_delete=models.CASCADE)
     id_restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     
-
 
 class Order(models.Model):
     id_order = models.AutoField(primary_key=True)
